feab230209644bdf13bc7a101df1a79e32d69cb6
Matthew de Asis <mattuda@gmail.com>
2016-10-06 17:22:33 -0700

    Added Recipes Model Added Recipe List View Added Recipe Single View Added Recipe Controller Signed-off-by:Matthew de Asis <mattuda@gmail.com>

--- Changelog.md
+++ Changelog.md
@@ -3,4 +3,10 @@
 ============================================
 Members: Matthew, Dennis, Injo, Haram, Lydia
 
+2016-10-06 5:18PM
+-Added Recipes Model
+-Added Recipe List View
+-Added Recipe Single View
+-Added Recipe Controller
+
 2016-10-05 7:25PM Added changelog
---
+++ application/views/recipe_single.php
@@ -0,0 +1,13 @@
+<div class="row">
+    <div class="span4">{itemName}
+        <hr>
+            {materialList}
+            <p>{name} : {amount} ({inStock})</p>
+            {/materialList}
+        <hr>
+        {form_open}
+        {amountToCraftForm}
+        {craftButton}
+        {form_close}
+    </div>
+</div>
