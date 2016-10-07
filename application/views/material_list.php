c5be5e4ba4bdf41808fb1216c6b24f6ffd6e190d
Haram Lee <haram.lee@hotmail.ca>
2016-10-07 14:34:59 -0700

    updated ui for material view

--- application/views/material_list.php
+++ application/views/material_list.php
@@ -1,8 +1,27 @@
-<div class ="row">
-{form_open}
-	{Materials_table}
-{form_close}
+<div id="navigation">
+    <ul>
+        <li><a href="/">Dashboard</a></li>
+        <li><a href="/admin">Administration</a></li>
+        <li><a href="/material">Material</a></li>
+        <li><a class="active" href="/recipe">Recipe</a></li>
+        <li><a href="/product">Product</a></li>
+    </ul>
 </div>
+<div id="content">
+    <div id="title">
+        <h1>Recipe</h1>
+    </div>
+
+    <div id="body">
+        <div class ="row">
+		{form_open}
+			{Materials_table}
+		{form_close}
+		</div>
+
+    </div>
+</div>
+
 
 
 <!--
