68c9a215aa3ea41ac8e148861ce15830f5d746ae
LingzhuYu <ylinz1227@gmail.com>
2016-10-06 17:26:50 -0700

    Added dashboard view, modify page style for stock

---
+++ application/views/dashboard.php
@@ -0,0 +1,18 @@
+<div id="navigation">
+	<ul>
+		<li><a class="active" href="/">Dashboard</a></li>
+		<li><a href="/admin">Administration</a></li>
+		<li><a href="/receiving">Receiving</a></li>
+		<li><a href="/production">Production</a></li>
+		<li><a href="/sales">Sales</a></li>
+	</ul>
+</div>
+<div id="content">
+	<div id="title">
+		<h1>Dashboard</h1>
+	</div>
+
+	<div id="body">
+		<p>Data here</p>
+	</div>
+</div>
