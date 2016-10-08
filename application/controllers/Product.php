1138ea979a9317fccde432c96229594cb5d3fccd
Injo Baik <baikinjo.28@gmail.com>
2016-10-07 22:52:46 -0700

    updated product controller
    Signed-off-by: Injo Baik <baikinjo.28@gmail.com>

--- application/controllers/Product.php
+++ application/controllers/Product.php
@@ -5,23 +5,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Product extends Application
 {
 
-	function __construct()
-	{
-		parent::__construct();
-	}
+    function __construct()
+    {
+        parent::__construct();
+    }
 
-	/**
-	 * Homepage for our app
-	 */
-	public function index()
-	{
-		// this is the view we want shown
-		$this->data['pagebody'] = 'stock_list';
-		$this->create_form('Products');
-		$this->render();
-	}
+    /**
+     * Homepage for our app
+     */
+    public function index()
+    {
+        // this is the view we want shown
+        $this->data['pagebody'] = 'sale_list';
+        $this->create_form('Products');
+        $this->render();
+    }
 
-	private function create_form($type) {
+    private function create_form($type) {
 
         //Open form
         $this->data['form_open'] = form_open('product/sales');
@@ -36,32 +36,59 @@ class Product extends Application
         // Add table rows
         foreach ($source as $record)
         {
-            $num_input = array('type' => 'number', 'value' => '0', 'class' => 'num-field', 'name' => 'quantity' . $record['id']);
+            $num_input = array('type' => 'number', 'value' => '0', 'class' => 'num-field', 'name' => $record['id']);
             
-            $items[] = array($record['name'], 
-                             $record['desc'], 
-                             $record['amount'], 
-                             $record['price'],
-                             form_input($num_input));
+            $items[] = array('<a href="/product/get/' .
+                              $record['id']. '">' .
+                              $record['name'] . '</a>',
+                              $record['desc'],
+                              $record['amount'],
+                              $record['price'],
+                              form_input($num_input));
         }
 
         //Generate the materials table
         $this->data[$type.'_table'] = $this->table->generate($items);
         //submit button
-        $this->data['order_button'] = form_submit('mysubmit', 'Order');
+        $this->data['order_button'] = form_submit('', 'Order');
         //clear form
-        $this->data['clear_data'] = form_reset('reset','Clear');
+        $this->data['clear_data'] = form_reset('','Clear');
         //close form
         $this->data['form_close'] = form_close();
     }
 
-    public function sales(){
+    public function get($id){
+        $this->data['pagebody'] = 'sale_single';
 
+        $source = $this->Products->get($id);
+
+        $items[] = array('Name', 'Description', 'Price');
+        $items[] = array($source['name'], $source['desc'], $source['price']);
+
+        $this->data['stock_table'] = $this->table->generate($items);
+        $this->render();
+    }
+
+    public function sales(){
+        $this->data['pagebody'] = 'sale_confirmation';
+        //$inventory[] = array();
         foreach($_POST as $post_name => $post_value){
             $this->Transactions->setProducts($post_name, $post_value);
+            $inventory[] = array('key' => $post_name, 'value' => $post_value);
         }    
-            
-        var_dump($this->Transactions->getProducts());
+
+
+
+        $result = array();
+        foreach($inventory as $source){
+
+            if($source['value'] != 0){
+                $record = $this->Products->get($source['key']);
+                $result[] = array('line' => "You ordered " . $source['value'] . ' ' .  $record['name'] . "</br>");
+            }
+        }
+        $this->data['result'] = $result;
+        $this->render();
     }
 
     public function clear() {
